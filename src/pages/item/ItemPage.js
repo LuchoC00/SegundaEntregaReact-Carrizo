d0181a5eaf9a9ed67ca039d56ed0ac7c8b3fd7c2
Luciano <raulo.carri@gmail.com>
2023-06-17 23:47:07 -0300

    listo

--- src/pages/item/ItemPage.js
+++ src/pages/item/ItemPage.js
@@ -1,20 +1,26 @@
 import React , {useState, useEffect} from 'react';
 import CardUser from '../../componets/CardUser/CardUser'
-
+import { useParams } from 'react-router-dom';
+import Button from '@mui/material/Button';
 
 const ItemPage = () => {
+
+    let {id} = useParams();
 
     const [user, setUser] = useState({});
 
     useEffect(()=>{
-        fetch(`https://fakestoreapi.com/products/${user.id}`)
+        fetch(`https://fakestoreapi.com/products/${id}`)
             .then(response => response.json())
             .then(json => setUser(json))
     },[])
 
     return (
-        <div>
+        <div style={{display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column"}}>
             <CardUser userData={user}/>
+            <Button variant="contained" disableElevation>
+                Price: ${user.price}
+            </Button>
         </div>
     )
 }
